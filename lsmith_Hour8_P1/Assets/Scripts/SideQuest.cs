d1888c3d8edcbb14ca63fd25ee538a41741260e4
larellchan <smithlarell2@gmail.com>
2024-10-19 18:00:26 -0700

    Removed the text for side quest

---
+++ lsmith_Hour8_P1/Assets/Scripts/SideQuest.cs
@@ -0,0 +1,4 @@
+// TakeDamage
+// Returns an int variable
+// The parameter is damageAmount
+// Health is created and is used with the value of 100.  The ''factory'' returns  the sum of health - damageAmount
---
+++ lsmith_Hour8and21/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            if (audioSource.isPlaying == true)
+            audioSource.Stop();
+            else
+            audioSource.Play();
+        }
+    }
+}
