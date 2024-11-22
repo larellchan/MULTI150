ee3661188a3a1d889888a28154802bab89080217
larellchan <smithlarell2@gmail.com>
2024-11-21 19:15:09 -0800

    Edited game critique

--- lsmith_Hour10/Assets/Scripts/GameCritique.cs
+++ lsmith_Hour10/Assets/Scripts/GameCritique.cs
@@ -1,11 +1,15 @@
 /* 1) Yes, the game is definitely too easy to beat.  All you're doing is pushing balls to the corners after all
 
-2) 2) This game would be easier if there was only one and the player had faster movement.  And this would be harder if there was were balls and the player was slower.  Or having a "death ball"
-    that destroys the player if they collide with it
+2) 2) This game would be easier if there was only one and the player had faster movement.  And this would be harder if there was more balls and the player was slower
 
-3) A wow factor for me would adding music when a player beats the game
+3) A wow factor for me would adding music to the game.  Can't have a game without music
 
 4) It was fun moving the ball around to the goal.  It's exactly like playing soccer.  Nothing in the game was tedious
+
+
+AFTER
+
+I made the player slower so you can't win the game within 3 seconds.  I added more balls and music as well.  Can't have a game without any music
 
 */
 
