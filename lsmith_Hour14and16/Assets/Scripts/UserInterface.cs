306b5ec1b35e5d8cdb45daafd9d84a47c162447f
larellchan <smithlarell2@gmail.com>
2024-12-01 20:53:01 -0800

    Completed Hour14
    I have to cram Hour16

---
+++ lsmith_Hour14and16/Assets/Scripts/UserInterface.cs
@@ -0,0 +1,9 @@
+/* 1: Added a background to the UI.  I choose this because I think it feels pretty good with the theme of the UI and eventually game
+ 
+ * 2: Added text/ buttons to the UI.  WHich includes the title, credits, instructions on the goal, and start button that will eventually transistion to a game scene
+ 
+ * Added music to capture the theme of this game. Was going to add a OST from Silent Hill but I opted not to 
+ 
+ * Attempted to add a static gif of a rotating skull as seen in my hour 14 assests but was unsuccessful of showing it in my scene.
+*/
+
