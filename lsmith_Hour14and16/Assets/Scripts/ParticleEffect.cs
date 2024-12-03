1bd23a4bca71f71c7f4f8469a98794561c7c8f24
larellchan <smithlarell2@gmail.com>
2024-12-02 23:33:56 -0800

    Locked in
    Spent all day finishing up Hour16!

---
+++ lsmith_Hour14and16/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,14 @@
+/*
+ Black flame pillars: For these four particle systems I adjusted the duration to 5, changed the start color/ lifetime to red, curve, made it a triangle shape, and gave them a texture
+
+Skull spiral: 
+Adjusted the start lifetime to 50, red start color, changed max particles to 1999, set the inherit velocity multipilier to 20, changed the over lifetime axes, changed the mesh and material to
+a skull
+
+Skull Wings B: Start speed is 5, Duration set to 10, Start color is black,  acvtivated prewarm, max particles set to 1,000, changed shape to hemisphere, activated trails, set the radius to 10 and thickened to 1, set to burst spread, gave new material, 
+
+Skull Wings R: Set the duration to 5, start color to red, changed shape to sphere, set ribbon mode for trails
+
+Particle System-chan!: Set the duration to 20, start speed to 15, gravity modifier to 55, 
+ */
+
