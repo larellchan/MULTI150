debcbb73c4b7af37ad3cc7145442b6e40071241d
larellchan <smithlarell2@gmail.com>
2024-12-07 23:01:48 -0800

    Finished Hour20
    Completo!

---
+++ lsmith_Hour20/Assets/Scrpts/GameCritic.cs
@@ -0,0 +1,19 @@
+/*This game was in between in my opionion.  It's like Subway Surfers but more slower.  It's harder to get obstacles fast when I'm moving slower.  Another reason why it's harder is because you
+ * have to collect powerups to extend the time count.  If you're missing them you will only be playing for 8 seconds.
+ * 
+ * One change to make it harder would be to make the obstacle bigger.  Which would make the players task of avoiding obstacles a tad harder.  One way to make the game easier would make
+ * increase the player speed.  Doing that can make collecting power ups more faster and having more play time
+ * 
+ * A wow factor would adding music to make the game more exciting.  Another one is to add diffrent kinds of obstacles.  Perhaps some zombies or something.
+ * 
+ * Running around in games is always fun even in this game.  Phasing out is cool
+ * 
+ * One of the tedious things is gathering powerups.  If you don't collect powerups the game will end.  Another tedious thing is avoiding obstacles and them slowing you down
+ * 
+ * 
+ * In the obstacle prefab I made it bigger, changed the color, and modified the particle system in a few ways
+ * 
+ * For the powerup prefab I changed the color of materials in the particle system and the prefab object
+ * 
+ * 
+  */
